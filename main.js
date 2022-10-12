67fcf1a5ba7619cd61a376124d20bc592e64cf09
Bruna N <88631869+bngbsb@users.noreply.github.com>
2022-10-12 16:25:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 function abreOverlay () {
+    alert("function abre overlay");
     document.getElementById("navOverlay_index").style.display = "block";
 }
 
 function fechaOverlay () {
+    alert("funcao fecha overlay");
     document.getElementById("navOverlay_index").style.display = "none";
 }
