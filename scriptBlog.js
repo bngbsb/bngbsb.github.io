ced5799fa2079a48b54497fa694faf72fd44f4bf
Bruna N <88631869+bngbsb@users.noreply.github.com>
2022-10-29 16:01:22 -0300

    Create scriptBlog.js

---
+++ scriptBlog.js
@@ -0,0 +1,12 @@
+function abreOverlay () {
+    alert("function abre overlay");
+    alert(document.getElementById("navOverlay_index").style.display); 
+    document.getElementById("navOverlay_index").style.display = "block";
+    alert(document.getElementById("navOverlay_index").style.display);
+    alert("acabou");
+}
+
+function fechaOverlay () {
+    alert("funcao fecha overlay");
+    document.getElementById("navOverlay_index").style.display = "none";
+}
