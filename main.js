8a56ebaf00c6b5937772e2f664b4b6c2301a7e29
Bruna N <88631869+bngbsb@users.noreply.github.com>
2022-10-29 16:38:10 -0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function abreOverlay () {
-    alert("function abre overlay");
-    document.getElementById("navOverlay_index").style.display = "block";
-}
-
-function fechaOverlay () {
-    alert("funcao fecha overlay");
-    document.getElementById("navOverlay_index").style.display = "none";
-}
