9e49ab5d6dc38ebaac5c253b7ffd9363ac08e45e
Bruna N <88631869+bngbsb@users.noreply.github.com>
2022-11-02 10:36:16 -0300

    Update scriptBlog.js

--- scriptBlog.js
+++ scriptBlog.js
@@ -1,18 +1,7 @@
-alert("stylesheet: " + document.getElementById("styleSheet_1").href);
-/*
-document.getElementById("styleSheet_1").href = "styleBlog_temaClaro.css";
-alert("stylesheet: " + document.getElementById("styleSheet_1").href);
-*/
-
 function abreOverlay () {
-    alert("function abre overlay");
-    alert(document.getElementById("navOverlay_index").style.display); 
     document.getElementById("navOverlay_index").style.display = "block";
-    alert(document.getElementById("navOverlay_index").style.display);
-    alert("acabou");
 }
 
 function fechaOverlay () {
-    alert("funcao fecha overlay");
     document.getElementById("navOverlay_index").style.display = "none";
 }
